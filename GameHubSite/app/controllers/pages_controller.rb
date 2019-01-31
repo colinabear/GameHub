c7ffd20d9ce5d0ee69af13f4e0475f1edee4cb46
Colin <cgabear@gmail.com>
2019-01-31 17:48:32 -0600

    Connect pages to allow visitation by link
    Can now travel to (domain)/account, (domain)/ (which brings up the home page), and (domain)/create_account.

---
+++ GameHubSite/app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def account
+
+  end
+  def home
+
+  end
+  def create_account
+
+  end
+end
--- GameHubSite/config/routes.rb
+++ GameHubSite/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+
+  get 'account' => 'pages#account'
+
+  get "create_account" => "pages#create_account"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
