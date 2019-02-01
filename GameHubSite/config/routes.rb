7597bbfdb219d310f690adef091cf5e864e67ca8
Colin <cgabear@gmail.com>
2019-01-31 18:18:21 -0600

    Add navbar to allow easy navigation of site

--- GameHubSite/config/routes.rb
+++ GameHubSite/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
+  root 'pages#home', as: 'home'
 
-  get 'account' => 'pages#account'
+  get 'account' => 'pages#account', as: 'account'
 
-  get "create_account" => "pages#create_account"
+  get 'create_account' => 'pages#create_account', as: 'create_account'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
