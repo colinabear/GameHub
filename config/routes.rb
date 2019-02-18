07469666a2e68ade935cefbc5e4ffd90007645e1
George Villaume <george@villaume.net>
2019-02-17 18:09:48 -0600

    Add create account page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'user/new'
+  get 'users/new'
+
+  get 'users/show'
 
   root 'pages#home', as: 'home'
 
-  get 'account' => 'pages#account', as: 'account'
+  get 'account', to: "pages#account"
 
-  get 'create_account' => 'pages#create_account', as: 'create_account'
+  get 'signup', to: "users#new"
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
