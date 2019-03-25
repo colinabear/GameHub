1076fcc3e29eaf896e43eac7b91166ff5cbc7bbc
Colin <cgabear@gmail.com>
2019-03-25 00:39:59 -0500

    Create functionality for Kanban board

---
+++ app/assets/javascripts/kanban.js
@@ -0,0 +1,22 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var toDoX = 0;
+var inProgX = 200;
+var recentCompletedX = 400;
+ctx.rect(0,0,100,60);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.rect(100,0,100,60);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.rect(200,0,100,60);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.rect(300,0,100,60);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.rect(10, 70, 200, 60);
+
+
+
+ctx.stroke();
