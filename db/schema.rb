9db0893f7af60c70930761b08955cb94dc7177c3
Colin <cgabear@gmail.com>
2019-04-01 21:11:24 -0500

    Add column to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190401030514) do
+ActiveRecord::Schema.define(version: 20190402020816) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20190401030514) do
     t.string "job_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "users", force: :cascade do |t|
