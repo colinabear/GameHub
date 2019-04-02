f60faec52bc1d5c5c8533e0922702abf680ec06e
George Villaume <george@villaume.net>
2019-04-01 20:03:53 -0500

    Add validations for models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+	validates :title, presence: true
+	validates :content, presence: true
+	validates :author, presence: true
 	belongs_to :user
 	has_many :comments
 end
