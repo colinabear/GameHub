5b56c718387bed2541660649388dce196751fe9f
mjieyoub <mjieyoub@gmail.com>
2019-04-07 15:50:19 -0500

    Fix pagination for tasks and add resume params

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,2 +1,12 @@
 class ResumeController < ApplicationController
+  def edit
+  end
+
+  def update
+    if @resume.update(resume_params)
+      redirect_to @resume
+    else
+      render 'edit'
+    end
+  end
 end
