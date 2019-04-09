1dd640197dde56561d43d29393b005da18f94eab
George Villaume <george@villaume.net>
2019-04-09 18:30:12 -0500

    Make sidebar and project pages better

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   end
 
   resources :projects do
+    get 'available_tasks', to: 'projects#available_tasks'
+    get 'hub', to: 'projects#hub'
     resources :tasks
     put 'progress_task', to: 'tasks#progress_task'
     put 'regress_task', to: 'tasks#regress_task'
