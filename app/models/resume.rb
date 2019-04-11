2b0d17380d19bd9dadcff1d73f1121b73108f884
George Villaume <george@villaume.net>
2019-04-11 01:10:53 -0500

    Really fix it

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,6 +3,7 @@ class Resume < ApplicationRecord
   validates :location, presence: true
   validates :phone_number, presence: true
   validates :biography, presence: true
+  validates :avatar, presence: true
   belongs_to :user
   mount_uploader :avatar, AvatarUploader
 end
