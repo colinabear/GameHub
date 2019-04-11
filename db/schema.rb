48ab19f4009be7c996e5694880245e33c59d357a
Colin <cgabear@gmail.com>
2019-04-10 19:48:46 -0500

    thing

--- db/schema.rb
+++ db/schema.rb
@@ -120,8 +120,8 @@ ActiveRecord::Schema.define(version: 20190410160623) do
     t.integer "story_rating", default: 0
     t.integer "puzzles_rating", default: 0
     t.integer "misc_rating", default: 0
-    t.string "messages_received"
-    t.string "messages_sent"
+    t.string "messages_received", array: true
+    t.string "messages_sent", array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
